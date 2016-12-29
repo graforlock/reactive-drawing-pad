0d48cfa59e3d005bf0f1bad85f68f251b387c23a
Maciej Sitko <maciej@neatcube.co.uk>
2016-12-29 16:07:38 +0100

    Ignored all js in all folders

--- src/enums/drawing.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Drawing;
-(function (Drawing) {
-    Drawing[Drawing["START"] = 0] = "START";
-    Drawing[Drawing["END"] = 1] = "END";
-})(Drawing || (Drawing = {}));
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Drawing;
