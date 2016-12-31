f5bf856463473455c4cab5f707a8f83b07dd2470
Maciej Sitko <maciej@neatcube.co.uk>
2016-12-31 12:07:02 +0000

    Example removed

--- src/main.ts
+++ src/main.ts
@@ -61,8 +61,6 @@ class DrawingPad {
             cLoop.loop(sLines.hold(initial));
 
             sLines.listen((coords: Coords): Line => new Line(canvas, coords));
-                //--> colorPicker.sColor.listen((value: any) => ctx.strokeStyle = value);
-
 
         });
     }
