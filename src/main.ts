a5cc96357776507e2a239fccc6ed72b5b731dd05
Maciej Sitko <maciej@neatcube.co.uk>
2017-01-03 23:19:48 +0000

    Before socket implementation

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,52 @@ import DrawingPad from './drawing-pad';
 import sInput from './elements/s-input';
 import sSubmit from './elements/s-submit';
 
-import { Cell, Stream, Tuple2 } from 'sodiumjs';
+import { Dimensions } from './interfaces';
 
-type NTuple2 = number | Tuple2<number, number>;
+import * as R from 'ramda';
+import {Cell, Stream, Tuple2, StreamLoop, Transaction} from 'sodiumjs';
 
-class DrawingApp
-{
-    public static main(DOMNode: HTMLElement = document.getElementById('controls-container')): void
-    {
-        const sHeightInput: sInput<number> = new sInput<number>('number', 200, DOMNode),
-              sWidthInput : sInput<number> = new sInput<number>('number', 300, DOMNode),
-              sButton     : sSubmit = new sSubmit('Create new canvas', DOMNode);
 
-        const height: Cell<number> = sHeightInput.value.map(value => Number(value)),
-              width : Cell<number> = sWidthInput.value.map(value => Number(value));
+type NTuple2<T, A> = number | Tuple2<T, A>;
+type Hash = string;
 
-        const sHeight: Stream<NTuple2> = Cell.switchS(height.map(v => sButton.sSink.map(() => v))),
-              sWidth: Stream<NTuple2> = Cell.switchS(width.map(v => sButton.sSink.map(() => v)));
+class DrawingApp {
+    private static state: StreamLoop<NTuple2<Dimensions, string>[]>;
 
-        const sDelta: Stream<NTuple2> = sHeight
-                                .merge(sWidth, (a: number, b: number): NTuple2 => new Tuple2(a, b));
-              sDelta.listen((v: any) => console.log(v));
+    public static main(DOMNode: HTMLElement = document.getElementById('controls-container')): void {
+        Transaction.run((): void => {
+            DrawingApp.state = new StreamLoop<Tuple2<Dimensions, Hash>[]>();
+
+            const sHeightInput: sInput<number> = new sInput<number>('number', 200, DOMNode),
+                  sWidthInput: sInput<number> = new sInput<number>('number', 300, DOMNode),
+                  sButton: sSubmit = new sSubmit('Create new canvas', DOMNode);
+
+            const height: Cell<number> = sHeightInput.value.map(value => Number(value)),
+                  width: Cell<number> = sWidthInput.value.map(value => Number(value));
+
+            const sHeight: Stream<NTuple2<Dimensions, Hash>> =
+                      Cell.switchS(height.map(v => sButton.sSink.map(() => v))),
+                  sWidth: Stream<NTuple2<Dimensions, Hash>> =
+                      Cell.switchS(width.map(v => sButton.sSink.map(() => v)));
+
+            const sDelta: Stream<NTuple2<Dimensions, Hash>> = sHeight
+                .merge(sWidth, (height: number, width: number): NTuple2<Dimensions, Hash> =>
+                    new Tuple2({height, width}, String(Math.random())));
+
+            DrawingApp.state.loop(
+                sDelta.snapshot(
+                    DrawingApp.state.hold([]),
+                    (delta: NTuple2<Dimensions, Hash>, accum: NTuple2<Dimensions, Hash>[])
+                        : NTuple2<Dimensions, Hash>[] => R.append(delta, accum))
+            );
+        });
+
+        this.state.listen((drawingPads: NTuple2<Dimensions, Hash>[]): void => {
+            drawingPads.forEach((drawingPad: Tuple2<Dimensions, Hash>, index: number): void =>
+            {
+                // Instantiate drawing pads
+            })
+        });
     }
 }
 
