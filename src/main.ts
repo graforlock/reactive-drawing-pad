fed2e8862db6262025f61cfbd1302f87c2a66c3c
Maciej Sitko <maciej@neatcube.co.uk>
2017-01-03 23:20:47 +0000

    Before socket implementation

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import DrawingPad from './drawing-pad';
 import sInput from './elements/s-input';
 import sSubmit from './elements/s-submit';
 
-import { Dimensions } from './interfaces';
+import {Dimensions} from './interfaces';
 
 import * as R from 'ramda';
 import {Cell, Stream, Tuple2, StreamLoop, Transaction} from 'sodiumjs';
@@ -19,16 +19,16 @@ class DrawingApp {
             DrawingApp.state = new StreamLoop<Tuple2<Dimensions, Hash>[]>();
 
             const sHeightInput: sInput<number> = new sInput<number>('number', 200, DOMNode),
-                  sWidthInput: sInput<number> = new sInput<number>('number', 300, DOMNode),
-                  sButton: sSubmit = new sSubmit('Create new canvas', DOMNode);
+                sWidthInput: sInput<number> = new sInput<number>('number', 300, DOMNode),
+                sButton: sSubmit = new sSubmit('Create new canvas', DOMNode);
 
             const height: Cell<number> = sHeightInput.value.map(value => Number(value)),
-                  width: Cell<number> = sWidthInput.value.map(value => Number(value));
+                width: Cell<number> = sWidthInput.value.map(value => Number(value));
 
             const sHeight: Stream<NTuple2<Dimensions, Hash>> =
-                      Cell.switchS(height.map(v => sButton.sSink.map(() => v))),
-                  sWidth: Stream<NTuple2<Dimensions, Hash>> =
-                      Cell.switchS(width.map(v => sButton.sSink.map(() => v)));
+                    Cell.switchS(height.map(v => sButton.sSink.map(() => v))),
+                sWidth: Stream<NTuple2<Dimensions, Hash>> =
+                    Cell.switchS(width.map(v => sButton.sSink.map(() => v)));
 
             const sDelta: Stream<NTuple2<Dimensions, Hash>> = sHeight
                 .merge(sWidth, (height: number, width: number): NTuple2<Dimensions, Hash> =>
@@ -37,14 +37,12 @@ class DrawingApp {
             DrawingApp.state.loop(
                 sDelta.snapshot(
                     DrawingApp.state.hold([]),
-                    (delta: NTuple2<Dimensions, Hash>, accum: NTuple2<Dimensions, Hash>[])
-                        : NTuple2<Dimensions, Hash>[] => R.append(delta, accum))
+                    (delta: NTuple2<Dimensions, Hash>, accum: NTuple2<Dimensions, Hash>[]): NTuple2<Dimensions, Hash>[] => R.append(delta, accum))
             );
         });
 
         this.state.listen((drawingPads: NTuple2<Dimensions, Hash>[]): void => {
-            drawingPads.forEach((drawingPad: Tuple2<Dimensions, Hash>, index: number): void =>
-            {
+            drawingPads.forEach((drawingPad: Tuple2<Dimensions, Hash>, index: number): void => {
                 // Instantiate drawing pads
             })
         });
